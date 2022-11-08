c78e6022102dff758325bda74119945d7f355768
meisReal <simas.cesnakauskas@gmail.com>
2022-11-08 09:58:24 +0200

    to do item style

--- react-crash-classc/src/components/ToDoItem.js
+++ react-crash-classc/src/components/ToDoItem.js
@@ -4,14 +4,20 @@ import PropTypes from 'prop-types'
 
 export class ToDoItem extends Component {
     getStyle = () => {
-        if (this.props.todo.completed) {
-            return {
-                textDecoration: 'line-through'
-            }
-        } else {
-            return {
-                textDecoration: 'none'
-            }
+        // if (this.props.todo.completed) {
+        //     return {
+        //         textDecoration: 'line-through'
+        //     }
+        // } else {
+        //     return {
+        //         textDecoration: 'none'
+        //     }
+        // }
+        return {
+            background: '#f4f4f4',
+            padding: '10px',
+            borderBottom: '1px #ccc dotted',
+            textDecoration: this.props.todo.completed ? 'line-through' : 'none'
         }
     }
 
