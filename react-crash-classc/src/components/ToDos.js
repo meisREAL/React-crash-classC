8c3e6f77a868db2f5262a6e557d62f0c908ff483
meisReal <simas.cesnakauskas@gmail.com>
2022-11-08 08:58:46 +0200

    initial commit

---
+++ react-crash-classc/src/components/ToDos.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class ToDos extends Component {
+    render() {
+        return (
+            <div className="">
+                <h1>stuff</h1>
+            </div>
+        );
+    }
+}
+
+export default ToDos
